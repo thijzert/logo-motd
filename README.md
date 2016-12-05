782e94bcbe3d3667618045747c68969ddf2ee4ae
Thijs van Dijk <thijs@hooi.gr>
2016-12-05 15:45:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+logo-motd is a small utility that renders a black and white image file to text using the [Unicode Block Elements](https://en.wikipedia.org/wiki/Block_Elements).
+Its intended use is to generate a company logo greeter in `/etc/motd`.
+
+Installation
+------------
+Just run:
+
+    go get -u github.com/thijzert/logo-motd/...
+
+Usage
+-----
+    logo-motd /path/to/image.png
+
+License
+-------
+This program and its source code are available under the terms of the BSD 3-clause license.
+Find out what that means here: https://www.tldrlegal.com/l/bsd3
---
+++ logo-motd.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"image"
+	"image/color"
+	_ "image/jpeg"
+	_ "image/png"
+	"log"
+	"os"
+)
+
+var pixels = map[fourB]string{
+	fourB{false, false, false, false}: " ",      // space
+	fourB{true, false, false, false}:  "\u2598", // quadrant upper left
+	fourB{false, true, false, false}:  "\u259d", // quadrant upper right
+	fourB{true, true, false, false}:   "\u2580", // upper half block
+	fourB{false, false, true, false}:  "\u2596", // quadrant lower left
+	fourB{true, false, true, false}:   "\u258c", // left half block
+	fourB{false, true, true, false}:   "\u259e", // ur/ll diagonal
+	fourB{true, true, true, false}:    "\u259b", // inv. quadrant lower right
+	fourB{false, false, false, true}:  "\u2597", // quadrant lower right
+	fourB{true, false, false, true}:   "\u259a", // ul/lr diagonal
+	fourB{false, true, false, true}:   "\u2590", // right half block
+	fourB{true, true, false, true}:    "\u259c", // inv. quadrant lower left
+	fourB{false, false, true, true}:   "\u2584", // lower half block
+	fourB{true, false, true, true}:    "\u2599", // inv. quadrant upper right
+	fourB{false, true, true, true}:    "\u259f", // inv quadrant upper left
+	fourB{true, true, true, true}:     "\u2588"} // full block
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) == 0 {
+		log.Fatalf("Usage: %s FILE", os.Args[0])
+	}
+
+	logger := log.New(os.Stderr, "MOTD", log.Llongfile)
+	out := os.Stdout
+	exit := 0
+
+	for _, file := range args {
+		if len(args) > 1 {
+			out.Write([]byte(file + "\n"))
+		}
+
+		f, err := os.Open(file)
+		if err != nil {
+			logger.Print(err)
+			exit = 1
+			continue
+		}
+		defer f.Close()
+
+		img, _, err := image.Decode(f)
+		if err != nil {
+			logger.Print(err)
+			exit = 1
+			continue
+		}
+
+		size := img.Bounds()
+		for y := size.Min.Y; y < size.Max.Y; y += 2 {
+			for x := size.Min.X; x < size.Max.X; x += 2 {
+				ff := fourB{
+					isBlack(img.At(x, y)),
+					isBlack(img.At(x+1, y)),
+					isBlack(img.At(x, y+1)),
+					isBlack(img.At(x+1, y+1))}
+
+				out.Write([]byte(pixels[ff]))
+			}
+			out.Write([]byte("\n"))
+		}
+	}
+
+	os.Exit(exit)
+}
+
+type fourB struct {
+	A, B, C, D bool
+}
+
+func isBlack(c color.Color) bool {
+	r, g, b, _ := c.RGBA()
+	if r > 0x1000 {
+		return false
+	}
+	if g > 0x1000 {
+		return false
+	}
+	if b > 0x1000 {
+		return false
+	}
+	return true
+}
